7b18111717f8ce71b74f9f86d2fd70e777a2430a
AgusRacca <agusrac2014@gmail.com>
2023-05-25 22:54:39 -0300

    Terminado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import About from "./components/About/About.jsx";
+import Cards from "./components/Cards/Cards.jsx";
+import Detail from "./components/Detail/Detail.jsx";
+import Form from "./components/Form/Form.jsx";
+import Nav from "./components/Nav/Nav";
+import Favorites from "./components/Favorites";
 
 function App() {
+  const [characters, setCharacters] = useState( [] );
+  const [access, setAccess] = useState( false );
+  
+  const EMAIL = 'agusrac2014@gmail.com'
+  const PASSWORD = 'cohorteft38'
+  
+  const navigate = useNavigate();
+
+  function login(userData){
+      if (userData.password === PASSWORD && userData.email === EMAIL) {
+        setAccess(true);
+        navigate('/home');
+   }
+  }
+
+  useEffect(() => {
+    !access && navigate('/');
+  }, [access]);
+
+  const onSearch = id => {
+    // copiado ejercicio 7
+    //Evitar duplicados:
+    const characterId = characters.filter( character => character.id === Number( id ) );
+    //console.log(characterId);
+    if(characterId.length) return alert('Ese ID ya se esta mostrando, por favor busca otro')
+    axios(`https://rickandmortyapi.com/api/character/${id}`).then(
+      ({ data }) => {
+        if (data.name) {
+          setCharacters((oldChars) => [...oldChars, data]);
+        } else {
+          window.alert("¡No hay personajes con este ID!");
+        }
+      }
+    );
+  };
+
+  const onClose = (id) => {
+    setCharacters(characters.filter((caracter) => caracter.id !== Number(id)));
+   };
+
+  const location = useLocation();
+  //console.log(location);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        location.pathname !== "/"
+        ? <Nav onSearch={ onSearch } />
+        : null
+      }
+      <hr />
+      <Routes>
+        <Route exact path="/" element={<Form login={login} />} />
+        <Route
+          path="/home"
+          element={<Cards characters={characters} onClose={onClose} />}
+        />
+        <Route path="/about" element={<About />} />
+        <Route path="/detail/:id" element={ <Detail /> } />
+        <Route path='/favorites' element={<Favorites onClose={onClose}/>} />
+      </Routes>
     </div>
   );
 }
